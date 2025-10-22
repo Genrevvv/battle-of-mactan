b1369a26b0e36e0bf0a3768e596ec8b182a3f3e4
Genrevvv <genrevvaguilar@gmail.com>
2025-10-22 21:36:51 +0800

    sana magkaroon ng dragon sa Philippines para labanan ang mga corrupt at makasakay ako para makalipad

--- js/cards.js
+++ js/cards.js
@@ -2,27 +2,27 @@ let cards = [
     {
         id: 1,
         content: 'Character 1',
-        backgroundImage: '/assets/images/1000x1350.png'
+        backgroundImage: '/assets/images/lapu-lapu.png'
     },
     {
         id: 2,
         content: 'Character 2',
-        backgroundImage: '/assets/images/1000x1350.png'    
+        backgroundImage: '/assets/images/lapu-lapu.png'    
     },
     {
         id: 3,
         content: 'Character 3',
-        backgroundImage: '/assets/images/1000x1350.png'    
+        backgroundImage: '/assets/images/lapu-lapu.png'    
     },
     {
         id: 4,
         content: 'Character 4',
-        backgroundImage: '/assets/images/1000x1350.png'    
+        backgroundImage: '/assets/images/lapu-lapu.png'    
     },
     {
         id: 5,
         content: 'Character 5',
-        backgroundImage: '/assets/images/1000x1350.png'
+        backgroundImage: '/assets/images/lapu-lapu.png'
     }
 ];
 
